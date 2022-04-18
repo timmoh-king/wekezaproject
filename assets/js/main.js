6c3fb8b727cdccbf69d004aefd85724da65fc143
timmoh-king <timothykariuki43@gmail.com>
2022-04-18 18:26:48 +0300

    changes

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-04-09 19:26:47
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-04-18 17:25:12
+ */
+//Display Submit form
+function submitForm(){
+    var submit=('Your information has been recieved for processing');
+}
+
+ 
+
+
+
+
+ 
