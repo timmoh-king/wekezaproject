cf4d39ddb0435b851c68b92667b1438c522060a2
timmoh-king <timothykariuki43@gmail.com>
2022-04-20 11:33:32 +0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,23 @@
  * @Author: Your name
  * @Date:   2022-04-09 19:26:47
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-04-18 17:25:12
+ * @Last Modified time: 2022-04-20 11:05:44
  */
-//Display Submit form
-function submitForm(){
-    var submit=('Your information has been recieved for processing');
+
+
+
+/*Display submit comment text*/
+
+document.getElementById("commentHere").onclick=function() {myFunction()};
+
+function myFunction(){
+document.getElementById("commentHere").innerHTML = '<p class="alert alert-light text-center" role="alert"> Your comment has been submitted </p>'
 }
 
- 
+/*Display submit join us form*/
 
+document.getElementById("joinForm").onclick=function() {myForm()};
 
-
-
- 
+function myForm(){
+document.getElementById("joinForm").innerHTML = '<p class="alert alert-light text-center" role="alert"> Your details have been submitted </p>'
+}
